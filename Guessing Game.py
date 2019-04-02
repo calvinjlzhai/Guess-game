be256e62749f79b058bd28a98ede75bd98f375a4
Calvin <46201774+calvinjlzhai@users.noreply.github.com>
2019-04-01 23:06:25 -0400

    Add files via upload
    Reorganzing the coding syntax and functional in Python version 3.0.

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,17 +1,18 @@
 secret_word = "Canada"; #The user needs to put in this word to win
-guess = "";
+guess = ""
 guess_count = 0; #guess count from 0
-guess_limit = 5;
-out_of_guess = False;
+guess_limit = 5
+out_of_guess = False
 
 while guess != secret_word and not(out_of_guess): # user input do not match the secret word and not out of guesses
     if guess_count < guess_limit:
-        guess = raw_input("Enter your word: "); #user input to guess the word.
-        guess_count +=1;
+        print("Hint: It is a country famous for the maple leaf!")
+        guess = input("Enter your word: "); #user input to guess the word.
+        guess_count +=1
     else:
         out_of_guess = True; #user is out of guess
 
 if out_of_guess:
-    print ("You lost! Please try again!");
+    print ("You lost! Please try again!")
 else:
-    print("You win!");
+    print("You win!")
